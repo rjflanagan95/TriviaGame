90fd2063be664b3594bd5af2b834ed9cf751050e
Robert Flanagan <robertflanagan95@gmail.com>
2018-10-23 16:56:17 -0400

    established questions as objects with T/F value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
 // storing display references in variables
 var timeDisplay = $("#time-remaining");
 var questionDisplay = $("#question");
-var optionsDisplay = $("#options");
+var option1 = $("#option1");
+var option2 = $("#option2");
+var option3 = $("#option3");
+var option4 = $("#option4");
 var startBtn = $("#start-game");
 
 var correctAnswers = 0;
@@ -10,9 +13,34 @@ var timedOutAnswers = 0;
 
 var beginGame = false;
 
+// defining questions as objects
+var q1 = {
+    question: "here's the first question",
+    ans1: {
+        answer: "answer 1",
+        value: true,
+    },
+    ans2: {
+        answer: "answer 2",
+        value: false,
+    },
+    ans3: {
+        answer: "answer 3",
+        value: false,
+    },
+    ans4: {
+        answer: "answer 4",
+        value: false,
+    }
+}
+
+// array of questions to cycle through
+var qArray = [q1];
+
 
 // reset the game, either at the beginning after clicking "start" or after a completed game
 function resetGame() {
+    startBtn.text("");
     console.log("===================================");
     console.log("game has been reset");
     console.log("===================================");
@@ -26,17 +54,18 @@ function resetGame() {
 };
 
 function runGame() {
-    question();
+    for (var i = 0; i < 1; i++) {
+        question(qArray[i])
+    }
 };
 
 
 // this function will run every time a new question is asked
-function question() {
+function question(qNumber) {
     var timer = 10;
     timeDisplay.text(timer + "s");
     // every second, the timer will decrement
     var intervalId = setInterval(decrement, 1000);
-
     function decrement() {
         timer--;
         console.log(timer)
@@ -51,17 +80,16 @@ function question() {
         clearInterval(intervalId);
     }
 
-    questionDisplay.text("first question");
-    optionsDisplay.text("here are some options");
+    // displaying the question and possible answers
+    questionDisplay.text(qNumber.question);
+    option1.append(qNumber.ans1.answer);
+    option2.append(qNumber.ans2.answer);
+    option3.append(qNumber.ans3.answer);
+    option4.append(qNumber.ans4.answer);
 }
 
 
 // start the game when the button is clicked
 $(document).ready(function() {
     startBtn.on("click", resetGame);
-
-    // if (beginGame === true) {
-    //     console.log("beginGame is true");
-    //     question();
-    // }
 })
