21bf89aa7083a5cd45ae5d6bd3ae89f776dc774e
Robert Flanagan <robertflanagan95@gmail.com>
2018-10-27 21:22:08 -0400

    q3 doesn't appear on screen, answer to q2 also applies to q3; after resetting the game, only q1 appears on screen and answer applies to all qs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,11 +16,12 @@ var incorrectAnswers = 0;
 var timedOutAnswers = 0;
 var qcount = 0;
 
-// this interval tracks the timer on the page
+// this interval tracks the timer
 var intervalId;
-// this interval tracks the actual time for the question
-var qInterval;
-
+var transitionInterval;
+var displayInterval;
+// timer currently set for 6 seconds, will change to 30 seconds for the final game
+var timer = 6;
 
 // defining questions as objects
 var q1 = {
@@ -33,15 +34,57 @@ var q1 = {
 }
 var q2 = {
     question: "here's the second question",
-    correct: "option3",
+    correct: "option4",
     ans1: "2-1",
     ans2: "2-2",
-    ans3: "2-3 (correct)",
-    ans4: "2-4",
+    ans3: "2-3",
+    ans4: "2-4 (correct)",
+}
+var q3 = {
+    question: "here's the third question",
+    correct: "option2",
+    ans1: "3-1",
+    ans2: "3-2 (correct)",
+    ans3: "3-3",
+    ans4: "3-4",
+}
+var q4 = {
+    question: "here's the fourth question",
+    correct: "option3",
+    ans1: "4-1",
+    ans2: "4-2",
+    ans3: "4-3 (correct)",
+    ans4: "4-4",
 }
 
 // array of questions to cycle through
-var qArray = [q1, q2];
+var qArray = [q1, q2, q3, q4];
+
+// reset the game, either at the beginning after clicking "start" or after a completed game
+function resetGame() {
+    startBtn.text("");
+    startBtn.hide();
+    console.log("==================================================");
+    console.log("game has been reset");
+    console.log("==================================================");
+    // resetting scores
+    $(".score-area").show();
+    timeDisplay.show();
+    qcount = 0;
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    timedOutAnswers = 0;
+    clearInterval(intervalId);
+    updateScore();
+
+    runGame();
+}
+
+// keeps the game running in between questions
+function runGame() {
+    updateScore();
+    nextQuestion();
+}
 
 // start the game when the button is clicked
 $(".score-area").hide();
@@ -58,20 +101,25 @@ function displayQuestion() {
     option3.text(qArray[qcount].ans3);
     option4.text(qArray[qcount].ans4);
 
-    // timer currently set for 10 seconds, will change to 30 seconds for the final game
-    var timer = 10;
-    timeDisplay.text(timer + "s");
+    var localTimer = timer;
+    timeDisplay.text(localTimer + "s");
     // every second, the timer will decrement
     intervalId = setInterval(decrement, 1000);
+
+    function stopTimer() {
+        clearInterval(intervalId);
+        // clearTimeout(transitionInterval);
+    }
+
     function decrement() {
-        timer--;
-        timeDisplay.text(timer + "s");
+        localTimer--;
+        timeDisplay.text(localTimer + "s");
+
 
         // if time is up, log the answer as "timed out"
-        if (timer === 0) {
+        if (localTimer === 0) {
             // increment qcount to keep track of the number of questions answered
             qcount++;
-            stop();
             timeDisplay.text("Time's up!");
             timedOutAnswers++;
             console.log("Correct answers: " + correctAnswers);
@@ -80,105 +128,83 @@ function displayQuestion() {
             console.log("qcount: " + qcount);
             updateScore();
             clearQuestion();
-            setTimeout(nextQuestion, 5000);
+            clearTimeout(transitionInterval);
+            transitionInterval = setTimeout(nextQuestion, 3000);
+            stopTimer();
         }
+        // when one of the answers is selected, check to see if the right answer was chosen
+        $(".answer-button").click(checkAnswer);
     }
     
-    function stop() {
-        clearInterval(intervalId);
-        // clearInterval(qInterval);
-    }
-
-    // clears the question and answers
-    // really just here as an intermediate step
-    function clearQuestion() {
-        questionDisplay.text("");
-        option1.text("");
-        option2.text("");
-        option3.text("");
-        option4.text("");
-    }
-
-    // when one of the answers is selected, check to see if the right answer was chosen
-    $(".answer-button").click(function() {
-
-        // the id of the option div is compared to the correct answer listed in the question object
-        if (this.id === qArray[qcount].correct) {
-            qcount++;
-            stop();
-            timeDisplay.text("That's right!");
-            correctAnswers++;
-            console.log("Correct answers: " + correctAnswers);
-            console.log("Incorrect answers: " + incorrectAnswers);
-            console.log("Timed out answers: " + timedOutAnswers);
-            console.log("qcount: " + qcount);
-            updateScore();
-            clearQuestion();
-            setTimeout(nextQuestion, 5000);
-        } else {
-            qcount++;
-            stop();
-            timeDisplay.text("That's wrong!");
-            incorrectAnswers++;
-            console.log("Correct answers: " + correctAnswers);
-            console.log("Incorrect answers: " + incorrectAnswers);
-            console.log("Timed out answers: " + timedOutAnswers);
-            console.log("qcount: " + qcount);
-            updateScore();
-            clearQuestion();
-            setTimeout(nextQuestion, 5000);
+    function checkAnswer() {
+        if (qcount < qArray.length) {
+            // the id of the option div is compared to the correct answer listed in the question object
+            if (this.id === qArray[qcount].correct) {
+                qcount++;
+                timeDisplay.text("That's right!");
+                correctAnswers++;
+                console.log("Correct answers: " + correctAnswers);
+                console.log("Incorrect answers: " + incorrectAnswers);
+                console.log("Timed out answers: " + timedOutAnswers);
+                console.log("qcount: " + qcount);
+                updateScore();
+                clearQuestion();
+                clearTimeout(transitionInterval);
+                transitionInterval = setTimeout(nextQuestion, 3000);
+                stopTimer();
+            } else {
+                qcount++;
+                timeDisplay.text("That's wrong!");
+                incorrectAnswers++;
+                console.log("Correct answers: " + correctAnswers);
+                console.log("Incorrect answers: " + incorrectAnswers);
+                console.log("Timed out answers: " + timedOutAnswers);
+                console.log("qcount: " + qcount);
+                updateScore();
+                clearQuestion();
+                clearTimeout(transitionInterval);
+                transitionInterval = setTimeout(nextQuestion, 3000);
+                stopTimer();
+            }
         }
-    });
+    }
+
+}
+
+// clears the question and answers
+function clearQuestion() {
+    questionDisplay.text("");
+    option1.text("");
+    option2.text("");
+    option3.text("");
+    option4.text("");
 }
 
 function nextQuestion() {
+    updateScore();
     // if all the questions have been asked, end the game
-    if (qcount >= qArray.length) {
+    if (qcount === qArray.length) {
+        console.log("qcount === qArray.length");
         endGame();
-    } else {
-        // wait 1/2 second, then display the question
-        setTimeout(displayQuestion, 500);
+    } else if (qcount < qArray.length) {
+        // display the next question
+        displayQuestion();
+        // clearInterval(displayInterval);
+        // displayInterval = setTimeout(displayQuestion, 500);
     }
 }
 
-// reset the game, either at the beginning after clicking "start" or after a completed game
-function resetGame() {
-    startBtn.text("");
-    startBtn.hide();
-    console.log("===================================");
-    console.log("game has been reset");
-    console.log("===================================");
-    // resetting scores
-    $(".score-area").show();
-    qcount = 0;
-    correctAnswers = 0;
-    incorrectAnswers = 0;
-    timedOutAnswers = 0;
-    updateScore();
-
-    runGame();
-}
-
 // update the score display
-// mostly an intermediate step to make sure game is updating correctly
 function updateScore() {
     correctDisplay.text("Correct: " + correctAnswers);
     incorrectDisplay.text("Incorrect: " + incorrectAnswers);
     timedOutDisplay.text("Unanswered: " + timedOutAnswers);
 }
 
-// keeps the game running in between questions
-function runGame() {
-    console.log("!!!! new game is running!!!!");
-
-    updateScore();
-
-    nextQuestion();
-};
-
 function endGame() {
-    console.log("========game has ended========");
-    stop();
+    console.log("==================================================");
+    console.log("game has ended");
+    console.log("==================================================");
     startBtn.show();
     startBtn.text("Click to play again!");
     questionDisplay.text("");
@@ -186,7 +212,7 @@ function endGame() {
     option2.text("");
     option3.text("");
     option4.text("");
-    timeDisplay.text("");
+    timeDisplay.hide();
 
     updateScore();
-};
+}
